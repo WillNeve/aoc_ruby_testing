3d02bbeff1cebb8bea2ea57c4600d8fe4b706ebb
William Neve <107778219+WillNeve@users.noreply.github.com>
2023-11-29 22:50:36 +0000

    Update challenge.rb

--- challenge.rb
+++ challenge.rb
@@ -3,7 +3,7 @@ target_sum = 2020
 
 def sort_entries(entries, target_sum)
   # return the product of the two entries that sum to our target_sum
-  # sort_entries([1,3,4,5,6], 5) ---> 5
+  # sort_entries([1,3,4,5,6], 5) ---> 4
   return nil if entries.empty?
 
   entries.each do |entry|
